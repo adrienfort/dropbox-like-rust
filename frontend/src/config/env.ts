5b751efdeef776c2ed18dbab05e565f35dedce35
Adrien Fort <adrien.fort01@gmail.com>
2025-01-20 18:24:15 +0100

    fix: brut value for backend url in frontend

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,12 +1,13 @@
-const env = (name: string, default_value?: string, required = true): string => {
-  const value = import.meta.env[name];
-  if (value === undefined) {
-    if (required) throw new Error(`Env var ${name} is not set`);
-    if (default_value !== undefined) return default_value;
-  }
-  return value;
-};
+// const env = (name: string, default_value?: string, required = true): string => {
+//   const value = import.meta.env[name];
+//   if (value === undefined) {
+//     if (required) throw new Error(`Env var ${name} is not set`);
+//     if (default_value !== undefined) return default_value;
+//   }
+//   return value;
+// };
 
-const BACKEND_URL = env("VITE_BACKEND_URL");
+// const BACKEND_URL = env("VITE_BACKEND_URL");
+const BACKEND_URL = "http://localhost:8080";
 
 export { BACKEND_URL };
